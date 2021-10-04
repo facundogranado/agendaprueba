34196203bd6ab2252263722c065d4dbd16c412f4
facundogranado <facundogranadouni@gmail.com>
2021-10-04 12:22:26 -0300

    prueba

---
+++ src/main/java/logica/AgendaMain.java
@@ -0,0 +1,12 @@
+package logica;
+
+import giu.Principal;
+
+public class AgendaMain {
+    public static void main(String[] args) {
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
